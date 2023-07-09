a55ef78a9b7395aa67b0029c8c830a54604a116f
jaewook <jewook89@nate.com>
2023-07-09 14:45:09 +0900

    add appbar icons

--- lib/main.dart
+++ lib/main.dart
@@ -19,111 +19,26 @@ class Grade extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.cyan[300],
         appBar: AppBar(
-          title: Text('OJ'),
+          title: Text('Drawer example'),
           centerTitle: true,
           backgroundColor: Colors.cyan[500],
           elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              print('cliecked');
+            },            
+            ),
+            actions: [
+              IconButton(onPressed: (){
+                print("shopping cart");
+              }, icon: Icon(Icons.shopping_cart)),
+              IconButton(onPressed: (){
+                print("search");
+              }, icon: Icon(Icons.search))
+            ],
+
         ),
-        body: const Padding(
-          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Center(
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/profile.jpg'),
-                    radius: 60.0,
-                  ),
-                ),
-            Divider(
-              height: 60.0,
-              color: Colors.black,
-              thickness: 0.5,
-              endIndent: 30.0,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'JAEWOOK',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'Power Level',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '14',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('using lightsaber',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                ),
-                )
-              ],),
-              Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('face hero tattoo',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                ),
-                )
-              ],),
-              Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('fire flames',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                ),
-                )
-              ],)
-          ]),
-        ));
+    );
   }
 }
