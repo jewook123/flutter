3823774b4f40d3bd50c69973c1eb16e1968b44dc
unknown <jewook89@nate.com>
2023-07-09 13:13:25 +0900

    Create harator page

--- lib/main.dart
+++ lib/main.dart
@@ -3,35 +3,127 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'OHJAEWOOK',   // 앱을 총칭하는 이름
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: MyHomePage(),
+      title: 'Charactor card',
+      home: Grade(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-
+class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  // 무언가를 혼자해낼수 있도록 발판을 마련해주다 , 도화지
-      appBar: AppBar(
-        title: Text('First app'),  // 앱바에 표시될 이름
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text('Hello'),
-            Text('Hello'),
-            Text('Hello')
+    return Scaffold(
+        backgroundColor: Colors.cyan[300],
+        appBar: AppBar(
+          title: Text('OJ'),
+          centerTitle: true,
+          backgroundColor: Colors.cyan[500],
+          elevation: 0,
+        ),
+        body: const Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/profile.jpg'),
+                    radius: 60.0,
+                  ),
+                ),
+            Divider(
+              height: 60.0,
+              color: Colors.black,
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'JAEWOOK',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'Power Level',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '14',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('using lightsaber',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0,
+                ),
+                )
+              ],),
+              Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('face hero tattoo',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0,
+                ),
+                )
+              ],),
+              Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('fire flames',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0,
+                ),
+                )
+              ],)
           ]),
-      ),
-    );
+        ));
   }
 }
